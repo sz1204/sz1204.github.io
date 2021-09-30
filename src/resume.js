c2c803098a30b33799ae7fc2e932b86a73809c32
Sarah Zhang <sz1204@ad.unc.edu>
2021-09-30 13:33:35 -0400

    Updated resume on site

--- src/resume.js
+++ src/resume.js
@@ -1,6 +1,18 @@
 const resume = `# Resume & Background
 
-## Education - UNC Chapel Hill
+## Education
+
+### Central Bucks East High School
+
+Relevant Courses: AP European History, AP U.S. History, AP Calculus AB, AP Chemistry, AP English Language, AP Psychology, AP Computer Science A, AP Statistics, AP Biology, AP Physics N/M, AP Spanish
+
+Extracurriculars: Founder & President of Art Therapy Club, Vice President of Key Club, President of Reading Olympics Club, National Honors Society, Spanish National Honor Society, Patient Transporter at Doylestown Hospital (~160 hrs), Volunteer at A Woman's Place (~350 hrs), CIT at James A. Michener Art Museum (~100 hrs)
+
+GPA: 4.45/4.0 W, 3.91/4.0 UW
+
+ACT: 35/36
+
+### University of North Carolina - Chapel Hill
 
 #### Bachelor of Arts, Computer Science
 
@@ -8,42 +20,40 @@ Courses: Intro to Programming & Data Science, Calculus of Functions of One Varia
 
 Programming Languages: Proficiency in Java. Studying HTML, CSS, Python, Javascript.
 
-
-
-
-
 #### Bachelor of Arts, Political Science
 
 Courses: Intro to American Democracy, Political Issues in Europe Today
 
-
+Extracurriculars: Webmaster for Undergraduate Student Government Executive Branch, CS+ Social Good, Webmaster for Hongbin 4 Mayor campaign, AASA
 
 
 ## Experience
-### Student Researcher at the Pennsylvania Biotechnology Center
+### Student Researcher at the Pennsylvania Biotechnology Center (2019-20)
 Worked under Dr. Roshan Thapa’s BSL-2 lab at the PA Biotechnology Center. Conducted an independent research project on the selective killing of hepatocellular carcinoma and other cancers by piperlongumine, utilizing techniques such as Trypan blue counting, performing caspase assays, measuring luminescence, and Western blotting. Performed weekly experiments over the course of six months, committing on average around 10-12 hours each week.
 
-### Campaign Volunteer & Web Designer
+### Campaign Volunteer & Web Designer (2020-21)
 Volunteered on local mayoral campaign under Friends of Lolly Hopwood for Mayor. Designed and coded website by self using HTML and CSS, canvassed in town, phone & text banked, and assisted in designing social media graphics. Worked additionally on two other websites for school board candidates (i.e. https://electdrmariam.com/) 
 Reference - Larissa Hopwood; (215) 275-4262; lollyhopwood@gmail.com
 
-### Founder & President of Youth 4 Unity
+### Founder & President of Youth 4 Unity (2020-21)
 Founded Youth 4 Unity, a student-led activism group that raises awareness on global social/humanitarian issues: (https://www.youth4unity.com). Organized marches, contacted local government officials, ran donation drives, and led online educational summits. Created a website, oversaw a monthly informative email newsletter, scheduled meetings and events, and spoke at TedX Arcadia on student activism work.
 
-### Founder of the Gen Z Daily
+### Founder of the Gen Z Daily (2020-21)
 Founded The Gen Z Daily, a student-run newspaper, on Instagram. Wrote & published daily news briefings simplified to be understandable to a high school student, designed graphics for social media. Ran a staff team of 16 to develop a website, create a marketing agenda, and write for the newspaper. Collaborated with other youth-led social media accounts.
 
-
-
+### Starbucks Barista (2020-21)
+Worked as a barista at local Starbucks on bar creating drinks, taking orders both through the café and on drive-thru, connect with + assist customers, and maintain a clean and organized store.
 
 
 
 ## Skills and Awards
 Awards: Presidential Scholars Award Semifinalist, Princeton Prize in Race Relations.
 
-Languages: Fluency in English. Limited working proficiency in Mandarin & Spanish.
+Languages: Fluency in English. Limited working proficiency in Mandarin Chinese  & Spanish.
 
-Skills: Public speaking, student leadership, political campaigns, social media.
+Skills: Public speaking, student leadership, web development, political campaigns, social media.
+
+Leadership: CTY Civic Leadership Institute, HOBY Eastern Pennsylvania + World Leadership Conference, UNC Richard Epps Emerging Leaders Program
 
 `;
 
