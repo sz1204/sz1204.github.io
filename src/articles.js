c1a4e3ad39ef13d257099887db78f690113e530b
Sarah Zhang <sz1204@ad.unc.edu>
2021-10-02 20:09:50 -0400

    bug fixes

--- src/articles.js
+++ src/articles.js
@@ -3,9 +3,11 @@ const articles = () => {
     return (
         <div>
         <h1>articles i've been featured in!</h1>
-        <p><b>The Washington Post:</b>Poll Workers at Voting Stations<a href="https://www.washingtonpost.com/local/education/student-poll-workers/2020/11/02/5a2060e4-1d0e-11eb-ba21-f2f001f0554b_story.html"></a></p>
+        <p><b>The Washington Post: </b>Poll Workers at Voting Stations</p>
+            <iframe src="https://www.washingtonpost.com/local/education/student-poll-workers/2020/11/02/5a2060e4-1d0e-11eb-ba21-f2f001f0554b_story.html">
+            </iframe>
         </div>
-    )
+        )
     };
     
     export default articles;
