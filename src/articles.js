891232ad2a56a88b111b8bd0da0173515cb2f36b
Sarah Zhang <sz1204@ad.unc.edu>
2021-10-02 20:29:59 -0400

    updated articles

--- src/articles.js
+++ src/articles.js
@@ -3,11 +3,63 @@ const articles = () => {
     return (
         <div>
         <h1>articles i've been featured in!</h1>
+        <center>
         <p><b>The Washington Post: </b>Poll Workers at Voting Stations</p>
-            <iframe src="https://www.washingtonpost.com/local/education/student-poll-workers/2020/11/02/5a2060e4-1d0e-11eb-ba21-f2f001f0554b_story.html" width="600" height="400">
+            <iframe src="https://www.washingtonpost.com/local/education/student-poll-workers/2020/11/02/5a2060e4-1d0e-11eb-ba21-f2f001f0554b_story.html" width="1200" height="600">
             </iframe>
+            <br />
         
-        
+            <p><b>Declarasian: </b>Activist Spotlight</p>
+            <iframe src="https://www.declarasian.org/activist-spotlights.html" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>What We Think Podcast: </b>2020 Election Reflection</p>
+            <iframe src="https://open.spotify.com/episode/0gVxl6XGt53isXML9JJ9pE?si=MMvxcvI6RTOFAYpy-iSjsQ&dl_branch=1" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>TedX Arcadia: </b>20/20 Vision - The Power of Student Activism</p>
+            <iframe src="https://www.ted.com/tedx/events/39190" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>Bucks County Courier Times: </b>Antiracism Education</p>
+            <iframe src="https://www.buckscountycouriertimes.com/story/news/2021/04/29/central-bucks-diwali-debate-underscores-lack-diversity-schools-students-say-bucks-county/4859668001/" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>WITF Radio: </b>2021 School Year Reflection</p>
+            <iframe src="https://www.witf.org/2021/06/11/loss-healing-resistance-and-hope-5-recent-grads-reflect-on-a-turbulent-year/" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>Bucks County Today: </b>Social Justice Reflection</p>
+            <iframe src="https://bucksco.today/2021/06/14/doylestown-high-school-graduates-social-justice/" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>Bucks County Courier Times: </b>George Floyd Memorial</p>
+            <iframe src="https://www.buckscountycouriertimes.com/story/news/2021/05/25/bucks-county-remembers-george-floyd-calls-more-police-reform/7421210002/" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>Bucks County Courier Times: </b>Support for Teachers Rally</p>
+            <iframe src="https://www.buckscountycouriertimes.com/picture-gallery/news/local/2020/08/16/central-bucks-back-to-school-rallies/5595023002/?fbclid=IwAR0Jru0KUh9NGIg8rbobGqUDz0FhX2pixK2wEYCT1p2wDCG3_fgR4S-MJtg" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>Bucks County Courier Times: </b>Princeton Prize for Race Relations</p>
+            <iframe src="https://education.piyasaa.com/bucks-county-students-combatting-racism-in-education-want/" width="1200" height="600">
+            </iframe>
+            <br />
+
+            <p><b>Daily Tar Heel: </b>Fridays for Future Climate Protest</p>
+            <iframe src="https://www.dailytarheel.com/article/2021/09/university-coal-plant-protest" width="1200" height="600">
+            </iframe>
+            <br />
+
+        </center>
         </div>
         )
     };
