d1199b5509a21b07129489f4bbe982d02db2ed0a
Sarah Zhang <sz1204@ad.unc.edu>
2021-10-02 20:14:03 -0400

    sizing changes for website embed

--- src/articles.js
+++ src/articles.js
@@ -4,8 +4,10 @@ const articles = () => {
         <div>
         <h1>articles i've been featured in!</h1>
         <p><b>The Washington Post: </b>Poll Workers at Voting Stations</p>
-            <iframe src="https://www.washingtonpost.com/local/education/student-poll-workers/2020/11/02/5a2060e4-1d0e-11eb-ba21-f2f001f0554b_story.html">
+            <iframe src="https://www.washingtonpost.com/local/education/student-poll-workers/2020/11/02/5a2060e4-1d0e-11eb-ba21-f2f001f0554b_story.html" width="600" height="400">
             </iframe>
+        
+        
         </div>
         )
     };
