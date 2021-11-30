b8fdaa312973e829919c8d9538d35a14b6fc3158
Sarah Zhang <sz1204@ad.unc.edu>
2021-11-30 15:29:03 -0500

    removed anime and music constants from homepage for professionality

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,18 +4,6 @@ import flowers from "./data/images/flowers.gif";
 import clouds from "./data/images/clouds.gif";
 
 const Gallery = () => {
-  const artists = [
-    "taylor swift - 'call it what you want'",
-    "lana del rey - 'beautiful people beautiful problems'",
-    "sza - 'broken clocks'",
-    "tyler, the creator - 'wusyaname'",
-    "wallows - 'just like a movie'",
-    "lorde - 'mood ring'",
-    "clairo - 'bags'",
-    "phoebe bridgers - 'graceland too'",
-    "arctic monkeys - 'i wanna be yours'",
-    "frank ocean - 'lost'",
-  ];
   const currently_reading = [
     "our time is now by stacey abrams",
     "the alignment problem by brian christian",
@@ -27,17 +15,6 @@ const Gallery = () => {
     "klara and the sun by kazuo ishiguro",
     "valley of the dolls by jacqueline susann",
     "how to be an antiracist by dr. ibram x kendi",
-  ];
-  const anime = [
-    "jujutsu kaisen",
-    "porco rosso",
-    "whisper of the heart",
-    "haikyuu!!",
-    "horimiya",
-    "words bubble up like soda pop",
-    "when marnie was there",
-    "weathering with you",
-    "demon slayer",
   ];
 
   return (
@@ -68,26 +45,6 @@ const Gallery = () => {
           <h4>what i've finished recently</h4>
           {finished_reading.map((books) => (
             <li>{books}</li>
-          ))}
-        </div>
-
-        <div id="music">
-          <h2>music i like!</h2>
-          <p><i>"she knows she lived through it to get to this moment."</i></p>
-          <img src={moon} alt="pink anime moon gif" />
-          <h4>artists + my favorite song of theirs;;-</h4>
-          {artists.map((artists) => (
-            <li>{artists}</li>
-          ))}
-        </div>
-
-        <div id="anime">
-          <h2>anime i like!</h2>
-          <p><i>"the world cannot live without love."</i></p>
-          <img src={clouds} alt="anime clouds gif" />
-          <h4>watched recently ;;-</h4>
-          {anime.map((anime) => (
-            <li>{anime}</li>
           ))}
         </div>
       </div>
