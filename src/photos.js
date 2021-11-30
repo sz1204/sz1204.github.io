e2814b4b9b68f0eea5650e3bb62b085a369a59e9
Sarah Zhang <sz1204@ad.unc.edu>
2021-11-30 16:15:13 -0500

    bug fixes on photo gallery

--- src/photos.js
+++ src/photos.js
@@ -1,5 +1,6 @@
 import great_wall from "./data/gallery/great_wall.jpeg";
 import golden_gate from "./data/gallery/golden_gate.jpeg";
+import salamanca from "./data/gallery/salamanca.jpeg"
 
 const photos = () => {
 
@@ -9,10 +10,7 @@ const photos = () => {
         <h1>Photo Gallery</h1>
             <p>Photos taken by me of beautiful places and beautiful things.</p>
         </center>
-
-        <img src={golden_gate} alt="Horizontal photo of the Golden Gate Bridge in San Francisco." />
-        <img src={great_wall} alt="Horizontal photo of the Great Wall of China. The wall is set amongst a backdrop of green mountains." />
-
+    
         </div>
     )
     };
